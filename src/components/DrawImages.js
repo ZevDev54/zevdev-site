c49d677b72d39ef89e1e3e0de1cf2851687524d4
zev <zevy.johnston@gmail.com>
2024-03-04 21:57:41 -0700

    Added link functionality to the game cards in draw images. Made it so hovering navbar link makes them underlinedd.

--- src/components/DrawImages.js
+++ src/components/DrawImages.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import games from '../images/gamethumbnails/thumbnail-list.json';
+import { Link } from 'react-router-dom';
 
 // C:\Users\zevyj\Desktop\Projects\Website Stuff\zevdev-reactwebsite-2\zevdev-site\src\
 
 const list = games.map((game) => {
     return (
+
         <div className='greencard gamecard'>
-            <img src={require('../images/gamethumbnails/'+game.thumbpath)} className='gamethumb' alt={'game thumnail for '+game.name}></img>
-            <h1 className='whitecard gametitle'>{game.name}</h1>
-            {/* <p>{game.pageslug}</p> */}
-            
-        </div>
+            <Link to="/lunarcheeseminingsim">
+                <img src={require('../images/gamethumbnails/' + game.thumbpath)} className='gamethumb' alt={'game thumnail for ' + game.name}></img>
+                <h1 className='whitecard gametitle'>{game.name}</h1>
+                {/* <p>{game.pageslug}</p> */}
+            </Link>
+        </div >
+
+
     )
 }
 )
