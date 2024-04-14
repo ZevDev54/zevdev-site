b89ec8c4b8e8fb26eb3b62b90424c1c5259453bc
ZevDev54 <zevy.johnston@gmail.com>
2024-04-14 14:56:58 -0600

    Changed markdown renderer to use .then() syntax

--- src/components/MarkdownRenderer.js
+++ src/components/MarkdownRenderer.js
@@ -6,19 +6,31 @@ import markTest from './test.md'
 import remarkGfm from 'remark-gfm'
 
 function MarkdownRenderer({ filePath }) {
-  const [markdown, setMarkdown] = useState('');
+  const [markdown, setMarkdown] = useState('loading description...');
 
-  useEffect( () => {
-    const fetchMD = async() => {
-      const file = await import(`../games/${filePath}.md`);
-      const response = await fetch(file.default);
-      const text = await response.text();
+  // useEffect( () => {
+  //   const fetchMD = async() => {
+  //     const file = await import(`../games/${filePath}.md`)
+  //     const response = await fetch(file.default);
+  //     const text = await response.text();
 
-      setMarkdown(text)
-    }
+  //     setMarkdown(text)
+  //   }
 
-    fetchMD()
-  }, [filePath])
+  //   fetchMD()
+  // }, [filePath])
+
+  useEffect(() => {
+    const fetchMD = () => {
+      import(`../games/${filePath}.md`)
+        .then(file => fetch(file.default))
+        .then(response => response.text())
+        .then(text => setMarkdown(text))
+        .catch(error => console.error('Error fetching markdown:', error));
+    };
+  
+    fetchMD();
+  }, [filePath]);
 
 
   // const markdown = "*just* a link: [link](https://zevdev.net)"
