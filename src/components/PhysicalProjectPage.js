1b2d4b0a45a7a9cb64feba52d1a6cc429c57729e
ZevDev54 <zevy.johnston@gmail.com>
2024-04-24 20:24:45 -0600

    Added MR synth stuff.

---
+++ src/components/PhysicalProjectPage.js
@@ -0,0 +1,10 @@
+
+
+
+export default function PhysicalProjectPage()
+{
+    return <div>
+        
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/5FAyFKfrF_0?si=fHs5X-3XgGsy0Y5d" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+    </div>
+}
