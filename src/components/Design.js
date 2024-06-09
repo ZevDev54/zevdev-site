867b2ba5579061a32cd7142a596f3f251ba57b6d
ZevDev54 <zevy.johnston@gmail.com>
2024-06-09 09:55:51 -0600

    Added youtube thumbnails, and zine content.

--- src/components/Design.js
+++ src/components/Design.js
@@ -7,8 +7,16 @@ export default function Design() {
 
     return(
         <>
-        <iframe src="https://drive.google.com/file/d/1mlMNNhPsQgboUu7oBLUwBuPDSMgvBVcc/preview" width="640" height="480" allow="autoplay"></iframe>
         <PageProjectCardBuilder projectsMeta={designProjects} containerDirectory={"images/design/"}></PageProjectCardBuilder>
+        
+        
+        
+        <div className="whitecard slidesContainer">
+        <h1 className='bigtext aligncenter'>Brand Identity Presentation</h1>
+        <p className='aligncenter projectDescription'>We used a prompt generator to create a fictional company which we would then make a logo and brand identity for. My company was called "Squirrel Forecasting", which boasted AI cloud computing powered weather prediction.</p>
+            <iframe  src="https://drive.google.com/file/d/1mlMNNhPsQgboUu7oBLUwBuPDSMgvBVcc/preview" width="640" height="480" allow="autoplay"></iframe>
+        </div>
+        
         </>
     );
     
--- src/components/PhysicalProjectPage.js
+++ src/components/PhysicalProjectPage.js
@@ -3,8 +3,8 @@
 
 export default function PhysicalProjectPage()
 {
-    return <div>
+    return <div className="whitecard slidesContainer">
         
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/5FAyFKfrF_0?si=fHs5X-3XgGsy0Y5d" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        <iframe width="1120" height="630" src="https://www.youtube.com/embed/5FAyFKfrF_0?si=fHs5X-3XgGsy0Y5d" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
     </div>
 }
